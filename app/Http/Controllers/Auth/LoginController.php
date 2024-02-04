9c0bfdbdf701af9e60d1b631b9efee84150a5af9
Pedro <pdiazstam@gmail.com>
2024-02-03 22:32:40 -0500

    Fixed - Error al autenticar.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,35 +44,42 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         try {
-            $response = Http::post('http://api1.v1.appomsv.com/login/veterinario', [
+            $response = Http::post(env('API1') . '/login/veterinario', [
                 'email' => $request->input("email"),
                 'password' => $request->input("password")
             ]);
-
-            $data = json_decode((string)$response->getBody(), true);
-
-            $user = new \App\Models\User();
-            $user->id = $data['id'];
-            $user->name = $data['nombres'];
-            $user->email = $data['email'];
-            $user->codVeterinario = $data["codVeterinario"];
-            $user->apellidos = $data["apellidos"];
-            $user->celular = $data["celular"];
-            $user->fijo = $data["fijo"];
-            $user->tipoDocumento = $data["tipoDocumento"];
-            $user->documento = $data["documento"];
-            $request->session()->put('user', $user);
-            Auth::login($user);
-
-            return redirect()->intended('mascotas');
-
+            if ($response->successful()) {
+                $data = json_decode((string)$response->getBody(), true);
+                if(empty($data)) {
+                    return redirect()->back()->with('error', 'Your credentials are incorrect. Please try again');
+                }
+                $user = new \App\Models\User();
+                $user->id = $data['id'];
+                $user->name = $data['nombres'];
+                $user->email = $data['email'];
+                $user->codVeterinario = $data["codVeterinario"];
+                $user->apellidos = $data["apellidos"];
+                $user->celular = $data["celular"];
+                $user->fijo = $data["fijo"];
+                $user->tipoDocumento = $data["tipoDocumento"];
+                $user->documento = $data["documento"];
+                $request->session()->put('user', $user);
+                Auth::login($user);
+                return redirect()->intended('mascotas');
+            } else {
+                if ($response->status() === 400) {
+                    return redirect()->back()->with('error', 'Invalid request. Please enter a username or a password.');
+                } elseif ($response->status() === 401) {
+                    return redirect()->back()->with('error', 'Your credentials are incorrect. Please try again');
+                }
+                return redirect()->back()->with('error', 'Something went wrong on the server.');
+            }
         } catch (\GuzzleHttp\Exception\BadResponseException $e) {
             if ($e->getCode() === 400) {
                 return redirect()->back()->with('error', 'Invalid request. Please enter a username or a password.');
             } elseif ($e->getCode() === 401) {
                 return redirect()->back()->with('error', 'Your credentials are incorrect. Please try again');
             }
-
             return redirect()->back()->with('error', 'Something went wrong on the server.');
         }
     }
